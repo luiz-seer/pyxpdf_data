c7cd1b3d36025ea1b72c1d1289b1178c667ab77f
Luiz Sutil <106616452+luiz-seer@users.noreply.github.com>
2022-07-12 19:59:41 +1000

    Update __init__.py

--- pyxpdf_data/__init__.py
+++ pyxpdf_data/__init__.py
@@ -43,11 +43,11 @@ def get_xpdfrc(force_rewrite=True):
         generate xpdfrc again even if it exists already. Helpful if somehow xpdfrc file
         got corrupted.
     """
-    xpdfrc_path = Path(get_python_lib(), "default.xpdf")
-    if (not xpdfrc_path.exists()) or force_rewrite:
-        xpdfrc = generate_xpdfrc()
-        with open(str(xpdfrc_path), "w") as fp:
-            fp.write(xpdfrc)
+#     xpdfrc_path = Path(get_python_lib(), "default.xpdf")
+#     if (not xpdfrc_path.exists()) or force_rewrite:
+#         xpdfrc = generate_xpdfrc()
+#         with open(str(xpdfrc_path), "w") as fp:
+#             fp.write(xpdfrc)
     return str(xpdfrc_path.absolute())
 
 
